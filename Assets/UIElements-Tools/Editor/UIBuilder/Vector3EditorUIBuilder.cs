9f88a1b01b1db01bddd6a9f181fbce70470b8cab
LAPTOP-LOJHGVFB\gaurav <gauravthailand@gmail.com>
2021-06-12 22:15:06 +1200

    adding vector 3 editor UI builder

---
+++ Assets/UIElements-Tools/Editor/UIBuilder/Vector3EditorUIBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+public class Vector3EditorUIBuilder : EditorUIBuilderCompType<Vector3>
+{
+    public override void Process(FieldInfo fieldInfo, object sourceObject, ref Dictionary<string, object> currentElementData)
+    {
+        Vector3Field field = new Vector3Field(fieldInfo.Name)
+        {
+            value = (Vector3)fieldInfo.GetValue(sourceObject)
+        };
+
+        field.RegisterValueChangedCallback((ChangeEvent<Vector3> e) => fieldInfo.SetValue(sourceObject, field.value));
+        field.labelElement.AddToClassList("label_min_width");
+
+        SetFieldElement(ref currentElementData, field);
+    }
+}
